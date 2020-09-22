cbecf9e1167e26ea5063c0bab8929bab0acbdf6b
Hannu Timonen <hannu.timonen@tuni.fi>
2020-09-22 16:24:49 +0300

    Animations etc.
    Animations etc.

---
+++ New Unity Project/Assets/Scripts/playerMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+    public Animator myAnimator;
+    public float speed = 10f;
+
+    // Update is called once per frame
+    void Update() 
+    {
+            if (Input.GetAxis("Horizontal") != 0)
+            {
+                myAnimator.SetBool("walking", true);
+               
+            }
+
+            else  if (Input.GetKey("up"))
+            {
+                myAnimator.SetBool("jumping", true);
+               
+            }
+            else 
+            {
+                myAnimator.SetBool("walking", false);
+                myAnimator.SetBool("jumping", false);
+            }
+
+          
+
+         
+
+
+            
+            
+           
+    }
+}
